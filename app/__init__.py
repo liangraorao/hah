6ec12e7b6f011fb7f400d749550b0b970b071eb6
liangraorao <aiworaorao@163.com>
2019-07-28 20:02:12 +0800

    数据库表的生成、db在flask核心对象上面的注册

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,14 @@ Created by Liangraorao on 2019/7/21 18:42
 filename : __init__.py.py
 """
 from flask import Flask
-
+from app.model.book import db
 def current_app():
      app = Flask(__name__)
      app.config.from_object('app.config')
      app.config.from_object('app.secure')
      register_blueprint(app)
+     db.init_app(app)
+     db.create_all(app=app)
      return app
 
 def register_blueprint(app):
